94e8c6071cec72741406a46bbdeb4a2317685a4f
James Cook <cookjws@gmail.com>
2025-01-12 12:07:49 +0000

    appears to work

--- src/ConcertinaSinpis.jl
+++ src/ConcertinaSinpis.jl
@@ -2,22 +2,39 @@ module ConcertinaSinpis
 
 struct ConcertinaSinpi{F}
   numerator::F
-  xs::UnitRange{Int}
-  function ConcertinaSinpi(num, xs)
-    @assert issorted(xs)
-    @assert length(xs) > 0
-    return new{typeof(num)}(num, xs)
+  az::Tuple{Int, Int} # the left and right hand integral bounds in normalised units
+  function ConcertinaSinpi(num, az)
+    @assert issorted(az)
+    return new{typeof(num)}(num, az)
   end
 end
 Base.:*(c::ConcertinaSinpi, x) = c.numerator(x) * (x / sinpi(x)) 
 
+"""
+    (c::ConcertinaSinpi)(t)
+
+Evaluate and sum the ConcertinaSinpi function at location `t` in [0, 1]
+where t is mapped to [0, 1] + az[1]:az[2], taking into account the sign
+of the denominator, sinpi(t + ax[1]:azp[2]), before finally dividing by
+|sinpi(t)|.
+
+...
+# Arguments
+- `c::ConcertinaSinpi(t`: 
+...
+
+# Example
+```julia
+```
+"""
 function (c::ConcertinaSinpi)(t)
   @assert 0 < t < 1
   n = c.numerator(t)
+  breaks = c.az[1]:c.az[2]
   output = zero(n)
-  sgn = sign(sinpi((c.xs[1] + c.xs[2]) / 2))
-  for i in 2:length(c.xs)
-    term = c.numerator(t + c.xs[i-1])
+  sgn = sign(sinpi((breaks[1] + breaks[2]) / 2))
+  for i in 2:length(breaks)
+    term = c.numerator(t + breaks[i-1])
     output += sgn * term
     sgn *= -1
   end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,16 +5,34 @@ f(x) = exp(-x^2)
 g(x) = sinpi(x)
 
 @testset "Concertinas.jl" begin
-  @show quadgk(ConcertinaSinpi(f, -100:100), 0.1, 0.9, rtol=1e-3)
-  @show Δ = 1e-14
-  @show quadgk(ConcertinaSinpi(x->exp(-(x-0.25)^2), -100:100), Δ, 1 - Δ, rtol=1e-6)
-  for i in 1:100
-    @test 2i ≈ quadgk(ConcertinaSinpi(sinpi, -i:i), Δ, 1 - Δ, rtol=1e-6)[1]
+  #@show quadgk(ConcertinaSinpi(f, -100:100), 0.1, 0.9, rtol=1e-3)
+  Δ = 1e-14
+  #@show quadgk(ConcertinaSinpi(x->exp(-(x-0.25)^2), -100:100), Δ, 1 - Δ, rtol=1e-6)
+  @testset "1/sin(x), Δ = $Δ" begin
+    for i in 1:100
+      @test 2i ≈ quadgk(ConcertinaSinpi(sinpi, (-i, i)), Δ, 1 - Δ, rtol=1e-6)[1]
+    end
   end
 
-  for i in [14], σ in 0:0.0001:1
-    Δ = 0.1^i
-    o = quadgk(ConcertinaSinpi(x->exp(-(x-σ)^2), -100:100), Δ, 1 - Δ, rtol=1e-8, atol=1e-8)[1]
-    #@show o
+  #for i in [14], σ in 0:0.0001:1
+  #  Δ = 0.1^i
+  #  o = quadgk(ConcertinaSinpi(x->exp(-(x-σ)^2), -100:100), Δ, 1 - Δ, rtol=1e-8, atol=1e-8)[1]
+  #  #@show o
+  #end
+
+  for i in -4:-1:-9
+    Δ = 10.0^i
+    expected = (log(tan((1 - Δ) * π / 2)) - log(tan(Δ * π / 2))) / 2π
+    result = quadgk(ConcertinaSinpi(identity, (0, 1)), Δ, 1 - Δ, rtol=1e-6)[1]
+    @testset "1 / sin(x), Δ = $Δ" begin
+      @test expected ≈ result
+    end
+
+    @testset "cos(x)^2 / sin(x), Δ = $Δ" begin
+      fcos²_sin(x) = (cos(x) + log(sin(x/2)) - log(cos(x/2))) / π 
+      expected = fcos²_sin((1 - Δ) * π) - fcos²_sin(Δ * π)
+      result = quadgk(ConcertinaSinpi(x->cospi(x)^2, (0, 1)), Δ, 1 - Δ, rtol=1e-6)[1]
+      @test expected ≈ result
+    end
   end
 end
